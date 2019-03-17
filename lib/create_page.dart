0aa9bf04b5b8f69159a0da6abcb306b839361fc0
Saurav <sauravhiremath@gmail.com>
2019-03-18 04:34:27 +0530

    small fix

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -26,5 +26,30 @@ class CreateNew extends StatelessWidget {
         radius: 48.0,
         child: Image.asset('assets/logo.png'),
       );
+
+      final name = TextFormField(
+        // controller: myController,
+        keyboardType: TextInputType.text,
+        autofocus: false,
+        decoration: InputDecoration(
+          hintText: 'Event Name',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        ),
+      );
+
+      return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            name,
+            SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
   }
 }
